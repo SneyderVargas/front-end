86b2c8a2084d756d90150ea91bde9ff9830dcc63
SneyderVargas <sneyr845@gmail.com>
2023-02-06 22:16:40 -0500

    Se realiza configuración de projet icon

--- Vue-Nuxt/Divisa/components/DrawerWeb.vue
+++ Vue-Nuxt/Divisa/components/DrawerWeb.vue
@@ -4,7 +4,9 @@
       <!--  -->
     </v-navigation-drawer>
     <v-app-bar>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer">
+        <Icon name="ic:outline-menu"/>
+      </v-app-bar-nav-icon>
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
   </div>
--- Vue-Nuxt/Divisa/nuxt.config.ts
+++ Vue-Nuxt/Divisa/nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            titleTemplate: '%s - divisas',
+            title: 'divisas',
+        }
+    },
     // ... other options
     modules: [
         // ...
         '@pinia/nuxt',
+        'nuxt-icon',
     ],
     css: ['vuetify/styles'],
     build: {
