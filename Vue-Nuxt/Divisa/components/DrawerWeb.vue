99e826ae5c9ee3598932af4777ee09cb623e4157
SneyderVargas <sneyr845@gmail.com>
2023-02-07 21:15:02 -0500

    update storage pina for drawer function get and set computed

--- Vue-Nuxt/Divisa/components/DrawerWeb.vue
+++ Vue-Nuxt/Divisa/components/DrawerWeb.vue
@@ -1,19 +1,27 @@
+<script lang="ts" setup>
+import { useMainStore } from '~~/stores/mainStore';
+import { defineComponent, ref, computed } from 'vue';
+const mainStore = useMainStore();
+// export default {
+//   data: () => ({ drawer: null }),
+// };
+// const drawer =  ref(false);
+
+const drawerComputada = computed({
+  get: () => mainStore.getDrawer,
+  set: (value) => { mainStore.setDrawer(value) }
+})
+</script>
 <template>
   <div>
-    <v-navigation-drawer v-model="drawer" fixed app>
+    <v-navigation-drawer v-model="drawerComputada" fixed app>
       <!--  -->
     </v-navigation-drawer>
     <v-app-bar>
-      <v-app-bar-nav-icon @click="drawer = !drawer">
-        <Icon name="ic:outline-menu"/>
+      <v-app-bar-nav-icon @click="drawerComputada = !drawerComputada">
+        <Icon name="ic:outline-menu" />
       </v-app-bar-nav-icon>
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
   </div>
 </template>
-  
-  <script>
-export default {
-  data: () => ({ drawer: null }),
-};
-</script>
--- Vue-Nuxt/Divisa/stores/mainStore.ts
+++ Vue-Nuxt/Divisa/stores/mainStore.ts
@@ -2,16 +2,23 @@ import { defineStore } from "pinia";
 
 export const useMainStore = defineStore('main', {
     state: () => ({
-        menssage: 'Mensaje Store'
+        menssage: 'Mensaje Store',
+        drawer: true
     }),
     actions: {
         setMenssage(newMessage: string) {
             this.menssage = newMessage
+        },
+        setDrawer(newDrawer: boolean){
+            this.drawer = newDrawer
         }
     },
     getters: {
         upper (): string {
             return this.menssage.toUpperCase()
+        },
+        getDrawer (): boolean {
+            return this.drawer
         }
     }
 })
